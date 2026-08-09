26c7fb57099e28624808d026af282dd4dd713ba2
Brandon-CSUN <Brando.sorto.36@my.csun.edu>
2026-08-08 21:09:16 -0700

    Created record models

---
+++ Models/RepositoryModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace GitHubTreadingCLI;
+
+public class RepositoryModel
+{
+    public record GitHubSearchRespone(
+        [property: JsonPropertyName("items")] List<Repository> Items
+    );
+
+    public record Repository(
+        [property: JsonPropertyName("name")] string Name,
+        [property: JsonPropertyName("owner")] RepositoryOwner Owner,
+        [property: JsonPropertyName("html_url")] string Html_url,
+        [property: JsonPropertyName("description")] string Description,
+        [property: JsonPropertyName("stargazers_count")] int Stars,
+        [property: JsonPropertyName("language")] string language
+    );
+
+    public record RepositoryOwner(
+        [property: JsonPropertyName("login")] string Login
+    );
+}
---
+++ Utilities/CliOptions.cs
@@ -0,0 +1,6 @@
+﻿namespace GitHubTreadingCLI;
+
+public class CliOptions
+{
+
+}
